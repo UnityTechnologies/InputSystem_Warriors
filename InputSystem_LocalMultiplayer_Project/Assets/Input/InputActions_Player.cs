014b6717fb5935d62c762a918042bbbd30fcbd18
andytouch <andyt@unity3d.com>
2020-01-27 15:27:08 +0100

    Upgrade Input System Package to 1.0.0-preview.4

--- InputSystem_LocalMultiplayer_Project/Assets/Input/InputActions_Player.cs
+++ InputSystem_LocalMultiplayer_Project/Assets/Input/InputActions_Player.cs
@@ -8,7 +8,7 @@ using UnityEngine.InputSystem.Utilities;
 
 public class @InputActions_Player : IInputActionCollection, IDisposable
 {
-    private InputActionAsset asset;
+    public InputActionAsset asset { get; }
     public @InputActions_Player()
     {
         asset = InputActionAsset.FromJson(@"{
