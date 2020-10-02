2bc10c7d40f888cc171964a2acd1848a04edf2b1
andytouch <andyt@unity3d.com>
2020-10-02 15:34:47 +0200

    Fix: Simplified PlayerVisualsBehaviour Setup

--- InputSystem_Warriors_Project/Assets/Scripts/Behaviours/Player/PlayerController.cs
+++ InputSystem_Warriors_Project/Assets/Scripts/Behaviours/Player/PlayerController.cs
@@ -43,7 +43,7 @@ public class PlayerController : MonoBehaviour
     {
         playerID = newPlayerID;
         playerAnimationBehaviour.SetupBehaviour();
-        playerVisualsBehaviour.SetupBehaviour(playerID, playerInput.devices[0].ToString());
+        playerVisualsBehaviour.SetupBehaviour(playerID, GetRawDevicePath());
         FindGameplayCamera();
 
     }
